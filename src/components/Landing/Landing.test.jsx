e574f104f5ffafbb2a45c70949be968167a56ccf
Jordi Turull <jordi.turull@mango.com>
2020-04-07 18:21:13 +0200

    Fix test

--- src/components/Landing/Landing.test.jsx
+++ src/components/Landing/Landing.test.jsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import Landing from './Landing';
 
 test('should render the title', () => {
-  const { getByText } = render(<Landing />);
+  const { getByText } = render(<MemoryRouter><Landing /></MemoryRouter>);
   expect(getByText('comp(Я)artim')).toBeInTheDocument();
 });
