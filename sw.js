6aaf3e6ca5825b0da73689d3875065b66235e6cb
Marc Ferret <marc.ferret@mango.com>
2020-04-07 17:33:18 +0200

    Fixed filestoCache of package

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ var cacheName = 'page';
 var filesToCache = [
   './',
   './index.html',
-  './hello-world.css',
-  './main.js'
+  './main.js',
+  './styles.css',
+  './food-icons.css'
 ];
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
