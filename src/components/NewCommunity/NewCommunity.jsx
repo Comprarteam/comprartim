4c8847958046a332983683b80e95ee17abef9413
Marc Ferret <marc.ferret@mango.com>
2020-04-08 11:29:03 +0200

    Added form to register new community

--- src/components/NewCommunity/NewCommunity.jsx
+++ src/components/NewCommunity/NewCommunity.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './NewCommunity.scss';
 
-const NewCommunity = () => <div data-testid="NEW_COMMUNITY">NewCommunity</div>;
+const NewCommunity = () => {
+  const [nameCommunity, setNameCommunity] = useState('');
+
+  const handleClickRegister = () => {
+    console.log(nameCommunity);
+  };
+
+  return (
+    <>
+      <div className={styles['community-title']} data-testid="NEW_COMMUNITY">Registra una nova comunitat de veïns</div>
+      <form>
+        <input
+          type="text"
+          value={nameCommunity}
+          placeholder="Introdueix el nom de la comunitat"
+          onChange={(e) => setNameCommunity(e.target.value)}
+        />
+        <div
+          role="button"
+          tabIndex={0}
+          className={styles['form-button']}
+          onClick={() => handleClickRegister()}
+          onKeyPress={() => handleClickRegister()}
+        >
+          Registrar comunitat
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default NewCommunity;
