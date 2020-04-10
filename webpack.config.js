fa95e76d0bd0aa9ca25426c2e7d45b781c8700b1
cescalonso <francesc.alonso@mango.com>
2020-04-10 16:08:04 +0200

    webpack.config.js does not have unused alias

--- webpack.config.js
+++ webpack.config.js
@@ -71,8 +71,6 @@ module.exports = () => {
     resolve: {
       alias: {
         react: path.join(__dirname, 'node_modules', 'react'),
-        ReactDOM: path.join(__dirname, 'node_modules', 'react-dom'),
-        React: path.join(__dirname, 'node_modules', 'react'),
       },
       extensions: ['.js', '.jsx', '.json'],
     },
