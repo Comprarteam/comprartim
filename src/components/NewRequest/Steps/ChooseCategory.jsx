d06154836dc51108892047d9f48ac9a1b82070e3
Jordi Turull <jordi.turull@mango.com>
2020-04-10 16:05:04 +0200

    Style new request

--- src/components/NewRequest/Steps/ChooseCategory.jsx
+++ src/components/NewRequest/Steps/ChooseCategory.jsx
@@ -4,23 +4,24 @@ import styles from './ChooseCategory.scss';
 
 const ChooseCategory = ({ onClickCategory }) => (
   <>
-    <div className={styles['categories-description']}>
-      El primer pas per crear una sol·licitud és
-      seleccionar de quina botiga tens una necessitat
+    <p className={styles['categories-description']}>
+      Selecciona a quina botiga vols anar a comprar.
+    </p>
+    <div className={styles['category-list']}>
+      {CategoriesList.map((category, idx) => (
+        <div
+          key={category.id}
+          role="button"
+          tabIndex={idx}
+          className={`${styles['category-item']} teal lighten-2 white-text`}
+          onClick={() => onClickCategory(category)}
+          onKeyPress={() => onClickCategory(category)}
+        >
+          <div className={`${styles['item-icon']} food-icon-${category.id}`} />
+          <div className={styles.name}>{category.name}</div>
+        </div>
+      ))}
     </div>
-    {CategoriesList.map((category) => (
-      <div
-        key={category.id}
-        role="button"
-        tabIndex={0}
-        className={`${styles['category-item']} indigo accent-1`}
-        onClick={() => onClickCategory(category)}
-        onKeyPress={() => onClickCategory(category)}
-      >
-        <div className={`${styles['item-icon']} food-icon-${category.id}`} />
-        <div>{category.name}</div>
-      </div>
-    ))}
   </>
 );
 
