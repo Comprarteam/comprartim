8793a4a333ea6c66cb66bb82bcb1a2b8add49514
Jordi Turull <jordi.turull@mango.com>
2020-04-10 16:04:20 +0200

    Add link in header

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Menu.scss';
 
 const Menu = () => {
-  const { communityId } = useParams();
-  const showPrinter = !!communityId;
+  let communityId = '1';
+
+  if (window.localStorage) {
+    communityId = window.localStorage.getItem('communityId');
+  }
   return (
     <div className={`indigo accent-4 ${styles['menu-wrapper']}`}>
       <div className={`container ${styles.container}`}>
-        <div className={styles.logo}>comp(Я)artim</div>
+        <Link
+          to={`/community/${communityId}/`}
+          alt="Imprimir"
+          className={`white-text ${styles.logo}`}
+        >
+          comp(Я)artim
+        </Link>
         <div className={styles.icons}>
-          {showPrinter && (
-            <Link to={`/community/${communityId}/print`} alt="Imprimir" className="white-text">
-              <i className="material-icons left">local_printshop</i>
-            </Link>
-          )}
+          <Link to={`/community/${communityId}/print`} alt="Imprimir" className="white-text">
+            <i className="material-icons left">local_printshop</i>
+          </Link>
           <i className="material-icons">person</i>
         </div>
       </div>
