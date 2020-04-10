32a454627e8be8485e430f467d444c20d9723e19
Marc Ferret <marc.ferret@mango.com>
2020-04-10 11:26:51 +0200

    Added chips to choose the products

--- src/components/NewRequest/Steps/ChooseProducts.jsx
+++ src/components/NewRequest/Steps/ChooseProducts.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styles from './ChooseProducts.scss';
 
 const ChooseProducts = ({ categorySelected, onClickConfirmRequest }) => {
   const products = ['500g de vedella', '200g de pernil salat', '3 butifarres'];
+  let instances;
+
+  useEffect(() => {
+    const elems = document.querySelectorAll('.chips');
+    const options = {
+      placeholder: 'Afegeix els productes',
+      secondaryPlaceholder: '+Productes',
+    };
+    instances = M.Chips.init(elems, options);
+    console.log(instances);
+  }, []);
 
   return (
     <>
       <p>{`El segon pas és afegir els productes que necessites del/de la ${categorySelected.name}`}</p>
-
+      <div className={`${styles['products-chips']} chips chips-placeholder`} />
       <div>{`Els productes de la teva sol·licitud per a ${categorySelected.name} són:`}</div>
       <div>
         <ul>
