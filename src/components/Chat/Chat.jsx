4a60548857cb8e0e1b1882e7b093e81624bc5810
cescalonso <francesc.alonso@mango.com>
2020-04-10 12:49:32 +0200

    chat works with firestore

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 import { Widget, addResponseMessage } from 'react-chat-widget';
 import { initializeFirebase, createChatRoom } from 'firebase-chat-ready-api';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyBYCPRz6ROZ6C_RVPst2pvYMWFt1zhB45g',
-  authDomain: 'comprartim.firebaseapp.com',
-  databaseURL: 'https://comprartim.firebaseio.com',
-  projectId: 'comprartim',
-  storageBucket: 'comprartim.appspot.com',
-  messagingSenderId: '450917423868',
-  appId: '1:450917423868:web:58ccb58da8e1035e6bd579',
-};
-
-initializeFirebase({
-  firebaseConfig,
-});
+initializeFirebase(
+  {
+    apiKey: 'AIzaSyBYCPRz6ROZ6C_RVPst2pvYMWFt1zhB45g',
+    authDomain: 'comprartim.firebaseapp.com',
+    databaseURL: 'https://comprartim.firebaseio.com',
+    projectId: 'comprartim',
+    storageBucket: 'comprartim.appspot.com',
+    messagingSenderId: '450917423868',
+    appId: '1:450917423868:web:58ccb58da8e1035e6bd579',
+  },
+);
 
 // firebase.firestore().settings({
 //   timestampsInSnapshots: true,
@@ -34,23 +31,26 @@ const userA = { userId: '1' };
 const userB = { userId: '2' };
 const members = [userA, userB];
 
+let newchatRoom;
 class Chat extends Component {
   static handleNewUserMessage(newMessage) {
     console.log(`New message incoming! ${newMessage}`);
     // Now send the message throught the backend API
-    this.newchatRoom.sendMessage(newMessage, userA, (err) => {
+    newchatRoom.sendMessage(newMessage, userA, (err) => {
       if (!err) console.log('message sent');
     });
   }
 
-  componentDidMount() {
-    this.newchatRoom = createChatRoom('Chat', members).then((result) => {
-      console.log(result);
-      return result;
-    }).catch((err) => {
-      console.log(err);
-    });
-    this.newchatRoom.getMessagesAndListen((message) => {
+  async componentDidMount() {
+    newchatRoom = await createChatRoom('Chat', members)
+      .then((result) => {
+        console.log(result);
+        return result;
+      }).catch((err) => {
+        console.log(err);
+      });
+    console.log(this.newchatRoom);
+    newchatRoom.getMessagesAndListen((message) => {
       console.log(message.body);
       addResponseMessage(message.body);
     });
