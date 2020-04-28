044b352882d8bf52ad77ce36ff8478f3d546c352
iXorx <iXorx@users.noreply.github.com>
2020-04-28 09:57:08 +0000

    Deploying to gh-pages from  @ d268a6d0773c55f39725f16b6b6b01170ab5da1b 🚀

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-var cacheName = 'comprartim-v1.2.0';
-var filesToCache = [
+const cacheName = 'comprartim-v1.3.0';
+const filesToCache = [
   './',
   './index.html',
   './main.js',
   './styles.css',
-  './food-icons.css',
-  './manifest.json'
+  './manifest.json',
+  './fonts',
+  './icons',
+  './styles/',
 ];
-self.addEventListener('install', function(e) {
+
+self.addEventListener('install', (e) => {
   console.log('[ServiceWorker] Install');
   self.skipWaiting();
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
-self.addEventListener('activate',  event => {
+self.addEventListener('activate', (event) => {
   var cacheKeepList = [cacheName];
   console.log('activate service worker');
   event.waitUntil(
@@ -27,13 +30,13 @@ self.addEventListener('activate',  event => {
           return caches.delete(key);
         }
       }));
-    })
+    }),
   );
 });
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request, { ignoreSearch:true }).then((response) => (
+      response || fetch(event.request)
+    )),
   );
 });
